08f311945c7099bc175d29667b66449f066cc525
ehgjs0415 <ehgjs0415@naver.com>
2025-09-14 21:17:53 +0900

    2주차실습과제완료(9.14일완료)

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller // 컨트롤러 어노테이션 명시
+public class DemoController{
+    @GetMapping("/hello") // 전송 방식 GET
+    public String hello(Model model) {
+        model.addAttribute("data", " 반갑습니다."); // model 설정
+        return "hello"; // hello.html 연결
+    }
+    @GetMapping("/hello2") // 전송 방식 GET + 새로운 매핑 추가
+    public String hello2(Model model) {
+        model.addAttribute("data1", " 송도헌님."); // model 설정
+        model.addAttribute("data2", " 반갑습니다."); // model 설정
+        model.addAttribute("data3", " 오늘."); // model 설정
+        model.addAttribute("data4", " 날씨는."); // model 설정
+        model.addAttribute("data5", " 매우 좋습니다."); // model 설정
+        return "hello2"; // hello2.html 연결
+    }
+} 
