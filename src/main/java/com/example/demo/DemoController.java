73f3606f63682f6db862b6eed9e63bdaa6a3db0d
ehgjs <ehgjs0415@naver.com>
2025-09-17 14:10:12 +0900

    9.17실습완료(과제는해야함)

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -18,4 +18,8 @@ public class DemoController{
         model.addAttribute("data5", " 매우 좋습니다."); // model 설정
         return "hello2"; // hello2.html 연결
     }
+    @GetMapping("/about_detailed")
+    public String about_detailed(Model model){
+        return "about_detailed";
+    }
 } 
