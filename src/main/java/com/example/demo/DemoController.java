45ba082041e9c2b615f7fd4ea8a594412ff1e5d8
ehgjs0415 <ehgjs0415@naver.com>
2025-09-22 22:18:36 +0900

    3주차실습문제해결(9.22)

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller // 컨트롤러 어노테이션 명시
 public class DemoController{
     @GetMapping("/hello") // 전송 방식 GET
